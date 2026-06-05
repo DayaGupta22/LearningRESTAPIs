db1f2270a3ffecf608698d26f4dbda5075d27366
DayaGupta22 <dayanandkumargupta08@gmail.com>
2026-06-05 11:00:34 +0530

    First APi Request Handle

---
+++ src/main/java/com/TechieSpring/LearningRESTAPis/LearningRestaPisApplication.java
@@ -0,0 +1,14 @@
+package com.TechieSpring.LearningRESTAPis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRestaPisApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(LearningRestaPisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TechieSpring/LearningRESTAPis/LearningRestaPisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechieSpring.LearningRESTAPis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningRestaPisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
